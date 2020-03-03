b6c04354d28715474cc1c599978cfed371eeda63
Your Name <you@example.com>
2020-03-03 08:08:03 +0300

    Add the add button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#delivery").click(function(){
+    $("#delivery").select(function(){
         $(".address").show();
         $("#delivery").hide();
     })
@@ -19,9 +19,9 @@ $(document).ready(function(){
     var total=0;
     $("input #add").click(function(event){
     event.preventDefault();
-       var pizzaSize=parseInt($("#size option:select").val()); 
-       var pizzaCrust =parseInt($("#crust option:select").val()); 
-       var pizzaTopping= parseInt($("#topping option:select").val());
+       var pizzaSize=parseInt($("#size option:selected").val()); 
+       var pizzaCrust =parseInt($("#crust option:selected").val()); 
+       var pizzaTopping= parseInt($("#topping option:selected").val());
 
        let newOrder = new Order(pizzaSize,pizzaCrust,pizzaTopping);
 
@@ -29,10 +29,10 @@ $(document).ready(function(){
 
     total+=price;
 
-    var Tsize= $("#size option:select").text();
-    var Tcrust= $("#crust option:select").text();
-    var Ttopping= $("#topping option:select").text();
-    var Tflavour= $("#flavour option:select").text();
+    var Tsize= $("#size option:selected").text();
+    var Tcrust= $("#crust option:selected").text();
+    var Ttopping= $("#topping option:selected").text();
+    var Tflavour= $("#flavour option:selected").text();
     var Tamount=document.getElementById("#amount").value;
 
     $("tbody").append("<tr>"+"<td>"+Tflavour+"</td>"+"<td>"+Tsize+"</td>"+"<td>" +Tcrust+ "</td>"+"<td>" + Tamount +"</td>" +"<td>"+Ttopping+ "</td>" +"<td>" +price +"</td>"+"</tr>")
@@ -40,16 +40,10 @@ $(document).ready(function(){
   });
 
   
-      $("#order").click(function(validation){
+      $("#order").click(function(event){
           $("#order").text(total);
           $("#last").show();
-
-          function validation(){      
-          var name=document.getElementById("#name").value;
-          var address=document.getElementById("#address").value;
-
-  confirm(name+" "+"you order has been placed and will be delivered at"+" "+address);
-      }
+          
   });
 
 });
